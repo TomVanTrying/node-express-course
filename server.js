2740cdc87a23519a799fe5950041e8e2ad1cbc64
Tom Van <Tom_Van@Trimble.com>
2022-02-02 11:42:07 -0700

    add dynamic GET route

--- server.js
+++ server.js
@@ -4,7 +4,19 @@ const express = require('express');
 // creates instance of express 
 const app = express();
 
+// words with colons in front of them in url are treated as variables 
+// (ie :id)
+app.get('/users/:id',function(req,res){
+	console.log(req.params.id)
+	res.json({
+		success: true,
+		message: 'got one user',
+		user: req.params.id
+	})
+})
+
 // Start up server locally on the port 
 app.listen(8002,function(){
     console.log("server is running")
     })
+
