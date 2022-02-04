32b537a52388ce5323c2b7fa8a407adbc7327452
Tom Van <Tom_Van@Trimble.com>
2022-02-03 17:15:24 -0700

    add login POST route

--- server.js
+++ server.js
@@ -1,11 +1,24 @@
+
+//  MOCK DATA 
+const mockUserData=[
+	{name:'Mark'},
+	{name:'Jill'}
+]
+
+
+
 // Search node modules for "express"
 const express = require('express');
 
 // creates instance of express 
 const app = express();
 
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
 // words with colons in front of them in url are treated as variables 
 // (ie :id)
+// GET 
 app.get('/users/:id',function(req,res){
 	console.log(req.params.id)
 	res.json({
@@ -14,6 +27,37 @@ app.get('/users/:id',function(req,res){
 		user: req.params.id
 	})
 })
+
+app.get('/users',function(req,res){
+	res.json({
+		success: true,
+		message: 'successfully got users. Nice!',
+		users: mockUserData
+	})
+})
+
+// POST 
+app.post('/login',function(req,res){
+    const username=req.body.username;
+    const password=req.body.password;
+
+    const mockUsername="billyTheKid";
+    const mockPassword="superSecret";
+
+    if (username===mockUsername && password===mockPassword){
+         res.json({
+              success: true,
+              message: 'password and username match!',
+              token: 'encrypted token goes here'
+         })
+    } else {
+         res.json({
+              success: false,
+              message: 'password and username do not match'
+         })
+    }
+})
+
 
 // Start up server locally on the port 
 app.listen(8002,function(){
